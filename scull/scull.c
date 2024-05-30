823abd6005f5ac2f3e263fc8d384d315b3ad95bf
ReMe-git <1242235737@qq.com>
2024-05-31 01:14:50 +0800

    add scull/scull.h

--- scull/scull.c
+++ scull/scull.c
@@ -2,6 +2,8 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
+#include "./scull.h"
+
 MODULE_LICENSE("DualBSD/GPL");
 
 static loff_t scull_llseek(struct file*, loff_t, int);
@@ -17,53 +19,68 @@ static struct file_operations scull_ops = {
 	.write = scull_write,
 	.open = scull_open,
 	.release = scull_release,
-};
+}; //scull file operations
+
+static dev_t scull_dev; //scull device number
+static int scull_major = SCULL_MAJOR; //scull major number
+static int scull_minor = SCULL_MINOR; //scull minor number
+static int scull_ndev = SCULL_NDEV; //scull device number
 
 static loff_t
 scull_llseek(struct file* filp, loff_t f_pos, int offset)
 {
 	return 0;
-}
+} //llseek
 
 static ssize_t 
 scull_read(struct file* filp, char __user* sp, size_t size, loff_t* offset)
 {
 	return 0;
-}
+} //read
 
 static
 ssize_t scull_write(struct file* filp, const char __user* sp, size_t size, loff_t* offset)
 {
 	return 0;
-}
+} //write
 
 static
 int scull_open(struct inode* np, struct file* filp)
 {
 	return 0;
-}
+} // open
 
 static 
 int scull_release(struct inode* np, struct file* filp)
 {
 	return 0;
-}
+} //release
 
 static
-int register_scull_devnum(dev_t major, dev_t minor)
+int register_scull_devnum(void)
 {
-	return 0;
-}
+	int res;
+
+	if (scull_major) {
+		scull_dev = MKDEV(scull_major, scull_minor);
+		res = register_chrdev_region(scull_dev, scull_ndev, "scull");
+	} //if give major number 
+	else {
+		res = alloc_chrdev_region(&scull_dev, scull_minor, scull_ndev, "scull");
+	} //alloc major number
+
+	return res;
+} //register scull device number
 
 static int __init scull_init(void)
 {
 	return 0;
-}
+} //scull init function
 
 static void __exit scull_exit(void)
 {
 
-}
+} //scull exit function
 
 module_init(scull_init);
 module_exit(scull_exit);
